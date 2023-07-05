d8cd6a95613ae76def90e998d6348e16bc64d82f
Renny <rennydan72@gmail.com>
2023-07-05 21:14:04 +0530

    remove toggle button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,17 +5,6 @@ function Nav() {
         <a className="navbar-brand" href="#" style={{ fontSize: "3rem" }}>
           ♰
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
       </div>
     </nav>
   );
